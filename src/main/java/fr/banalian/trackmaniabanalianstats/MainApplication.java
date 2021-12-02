989510137c1bc5f286f845002374d6a214951291
Lilian Pouvreau <64379456+Banalian@users.noreply.github.com>
2021-12-02 11:49:23 +0100

    feat(UI): Added icon

--- src/main/java/fr/banalian/trackmaniabanalianstats/MainApplication.java
+++ src/main/java/fr/banalian/trackmaniabanalianstats/MainApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +14,9 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("mainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 800);
-        stage.setTitle("Banalian's Trackmania Stats");
+        stage.setTitle("Banalian's stat tracker");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("file:img/icon.png"));
         stage.show();
 
 
